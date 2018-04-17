422febc2b08fc42625b94db97db44a58cdc3264c
Luuk <luuknoverraz@gmail.com>
2018-04-17 14:11:39 +0200

    Opdracht 4 gemaakt (ik ga voor elke opdracht trouwens een aparte folder maken, om het overzichtelijk te houden)

--- script.js
+++
@@ -1,6 +0,0 @@
-var x = 30;
-alert("wat is dit? ohh... het is een " + typeof(x));
-x = "dipje"
-alert("en dit is een " + typeof(x) + " dus...")
-x = true;
-alert("en het is als laatste een " + typeof(x) + "!")
