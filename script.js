5d15953992c921412eb2003499243c5f6a2ebfc2
Luuk <luuknoverraz@gmail.com>
2018-04-17 14:05:17 +0200

    Een .js toegevoegd inplaats van een <script> tag en een alert programma dat 3 datatypen onderscheidt toegevoegd.

---
+++ script.js
@@ -0,0 +1,6 @@
+var x = 30;
+alert("wat is dit? ohh... het is een " + typeof(x));
+x = "dipje"
+alert("en dit is een " + typeof(x) + " dus...")
+x = true;
+alert("en het is als laatste een " + typeof(x) + "!")
