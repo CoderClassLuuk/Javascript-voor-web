86d2f5cd14d544148412db8fcbfb98861fa0d25c
Luuk <luuknoverraz@gmail.com>
2018-04-21 13:28:43 +0200

    Opdracht 1, 2 en 3 gemaakt.

---
+++ leskaart-2/opdracht-2/script.js
@@ -0,0 +1,3 @@
+var mijnElement = document.getElementById("para1");
+alert(mijnElement.innerHTML);
+alert(mijnElement.outerHTML);
---
+++ leskaart-2/opdracht-3/script.js
@@ -0,0 +1,4 @@
+function myFunction() {
+  var naam = prompt("Hoe heet jij?");
+  document.getElementById("groet").innerHTML = naam;
+}
