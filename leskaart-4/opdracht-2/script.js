569a06c0c95ca82c8a006aeaf3ee6bda21ef8d8f
Luuk <31882018+CoderClassLuuk@users.noreply.github.com>
2018-06-25 10:07:08 +0200

    Opdracht 2 javascript

---
+++ leskaart-4/opdracht-2/script.js
@@ -0,0 +1,6 @@
+function maakParagraafOp () {
+  var textChanged = document.getElemenById("text")
+  textChanged.style.backgroundColor = "yellow";
+  textChanged.style.fontSize = "12px";
+  textChanged.style.fontFamily = "verdana"
+}
