7e362747ca1ffd3d0fee1a92a1eb06ab9be2c91b
Luuk <31882018+CoderClassLuuk@users.noreply.github.com>
2018-06-29 09:54:16 +0200

    Verkeerde plek

--- leskaart-5/script.js
+++
@@ -1,9 +0,0 @@
-function mijnFunctie() {
-      var stuk = document.getElementById("paragraaf1");
-      console.log(stuk.innerHTML);
-      var titel = document.getElementById("titel");
-      tekst = "De tekst en kleur van de titel is net veranderd!";
-      titel.innerHTML = tekst;
-      titel.style.color = "steelblue";
-      stuk.innerHTML = "Dingen werken!";
-}
